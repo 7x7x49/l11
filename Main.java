4facfd7d6c92a8271d20963fba2971c6db3bbdc2
7x7x49 <98600734+7x7x49@users.noreply.github.com>
2022-04-23 10:16:10 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,21 @@
 package com.company;
 
 import java.util.Scanner;
+
 import java.util.regex.Matcher;
+
 import java.util.regex.Pattern;
+
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("This program will help Уou find a valid IP address. Enter something:");
-        String ip =in.nextLine();
+        String ip = in.nextLine();  //cчитывание целого введенного пользователем слова или фразы с консоли
 
         Pattern pattern = Pattern.compile("(((25[0-5]\\.)|(2[0-4]\\d\\.)|(1[0-9]{2}\\.)|([1-9][0-9]\\.)|([0-9]\\.)){3})((25[0-5])|(2[0-4]\\d)|(1[0-9]{2})|([1-9][0-9])|([0-9]))");
-        Matcher match = pattern.matcher(ip);
+        Matcher match = pattern.matcher(ip); //хранение результата
 
         if (match.find()){
             System.out.println("Valid IP address: " + match.group());
